9850c7398000a8d8ad18cc8feff549b8c3b6b6b3
Victor Avila <avilavictor1570@gmail.com>
2020-07-16 03:00:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.insert(1, "and").join(" ")
   elsif array.size > 2
-    array.last.insert(2, "and").join(", ")
+    array.insert[0](2, "and").join(", ")
   end
 end
